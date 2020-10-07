1e34135654544b8e0688810c17df9116bed7b5f5
Amina Nadeem <amina.nadeem@daviestech.org>
2020-10-07 15:33:23 +0000

    done

--- Question2.java
+++ Question2.java
@@ -5,7 +5,7 @@
 class Question2 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+        
 
     }
 }
--- Question3.java
+++ Question3.java
@@ -5,7 +5,7 @@
 class Question3 {
     public static void main(String[] args) {
 
-        /* Write your code here */
+        
 
     }
 }
